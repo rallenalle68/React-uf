5177f3f997d4dc517c4178bed60fb8a24cae4d8c
Rasmus Seppänen <rasmussep@gmail.com>
2020-09-02 18:42:10 +0200

    Loggo update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Logo from "./MobitLogo.png"
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <div><img className="mobitLogo" src={Logo} alt="website logo" /></div>
     </div>
   );
 }
